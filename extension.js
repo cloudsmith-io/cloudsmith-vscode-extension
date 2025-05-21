fd6a74197715bf8691d761ca790b340e816b559a
Colin Moynes <cmoynes@Colins-MacBook-Pro.local>
2025-05-21 13:45:11 +0100

    Changed notification text and command value

--- extension.js
+++ extension.js
@@ -12,16 +12,16 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "cloudsmith-vscode-extension" is now active!');
+	console.log('Congratulations, your extension "Cloudsmith VSCli" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('cloudsmith-vscode-extension.helloWorld', function () {
+	const disposable = vscode.commands.registerCommand('cloudsmith-vscode-extension.cloudsmithAuth', function () {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from cloudsmith-vscode-extension!');
+		vscode.window.showInformationMessage('Hello from Cloudsmith!');
 	});
 
 	context.subscriptions.push(disposable);
