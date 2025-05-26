7095cd8f31202971b3743cdf4ac7ee8db7915958
Colin Moynes <cmoynes@Colins-MacBook-Pro.local>
2025-05-26 16:35:59 +0100

    using Cloudsmith API directly now instead of CLI. Repo List quickpick working

--- extension.js
+++ extension.js
@@ -1,50 +1,58 @@
 // The module 'vscode' contains the VS Code extensibility API
-
-
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
-const exec = require('child_process').exec;
 
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 
 /**
  * @param {vscode.ExtensionContext} context
  */
 async function activate(context) {
 
-	const repos = exec('cloudsmith repos list -F pretty_json ', (err, stdout) => {
-		if (err) {
-			vscode.window.showInformationMessage('Failed to query repositories.');
-			return;
+	// Cloudsmith API Fetch All Repos URL
+	const apiReposURL = 'https://api.cloudsmith.io/v1/repos';
+	const apiKey = '8759814c39d066a104f9b6c50074cc223b3d1e42';
+	var t = '';
+
+	const requestOptions = {
+		method: 'GET',
+		headers: {
+			'Authorization': `Bearer ${apiKey}`,
+		},
+	};
+
+	const url = "https://api.cloudsmith.io/v1/repos";
+	try {
+		const response = await fetch(url, requestOptions);
+		if (!response.ok) {
+		throw new Error(`Response status: ${response.status}`);
 		}
-		else {
-			const data = JSON.parse(stdout)
-			console.log(res)
-			const res = data.map(
-				repo => {
-					return {
-						label: repo.data.name,
-						detail: repo.data.cdn_url,
-					}
-				}
-			)
-			return data
-		}
-	})
+		t = await response.json();
+	} catch (error) {
+		console.error(error.message);
+	}
+	// Map the json objects to be used by extensions QuickPick
+	const items = t.map(
+		repo => {
+			return {
+				label: repo.name + '(' + repo.repository_type_str + ')',
+				detail: repo.namespace,	
+				link: repo.self_html_url	
+			}
+		})
 
 	let reposList = vscode.commands.registerCommand('cloudsmith-vscode-extension.cloudsmithReposList',
 		async function () {
-			const repo = await vscode.window.showQuickPick(repos, {
-				matchOnDetail: true
+			const repo = await vscode.window.showQuickPick(items, {
+				placeHolder: "Your Cloudsmith repositories",
+				matchOnDetail: true,
 			})
-			console.log(repos)
+			if (repo == null) return
+			console.log(repo.link)
+
+			vscode.env.openExternal(repo.link)
 		});
 
 
 	let auth = vscode.commands.registerCommand('cloudsmith-vscode-extension.cloudsmithAuth', function () {
-		// The code you place here will be executed every time your command is executed
 		exec('cloudsmith auth -o cloudsmith', (err, stdout, stderr) => {
 			if (err) {
 				vscode.window.showInformationMessage('Failed to run command:', stderr);
@@ -54,17 +62,10 @@ async function activate(context) {
 			console.log(`stdout: ${stdout}`);
 			console.log(`stderr: ${stderr}`);
 		});
-		// Display a message box to the user
-		//vscode.window.showInformationMessage('Hello from Cloudsmith!');
 	});
 
 	let docs = vscode.commands.registerCommand('cloudsmith-vscode-extension.cloudsmithDocs', function () {
-		// The code you place here will be executed every time your command is executed
-
 		vscode.env.openExternal("https://help.cloudsmith.io/docs/welcome-to-cloudsmith-docs")
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Opening the Cloudsmith Docs website in your browser!');
 	});
 
 	context.subscriptions.push(auth, docs, reposList);
