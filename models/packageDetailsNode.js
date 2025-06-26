24398234c82fba4236162066d626d237b70bccda
colinmoynes <cmoynes@cloudsmith.io>
2025-06-26 10:22:43 +0100

    big changes, beautiful changes. the most big beautiful changes anyone has ever made...again

--- models/packageDetailsNode.js
+++ models/packageDetailsNode.js
@@ -4,41 +4,49 @@ const path = require('path');
 
 class PackageDetailsNode {
 	constructor(detail) {
-		;
 		this.label = detail;
 	}
 
 	getTreeItem() {
 		let iconPath = ''
-		let detail = this.label
-		const text = detail.label
+		let detail = this.label	
+		let id = detail.label.id
+		let value = detail.label.value
 
-		if (text.includes("Slug")) {
+		if (value instanceof Array){ //check if value is an array (tags.info and tags.version combined)
+			value = String(value) //set value to string type
+		}
+		else {
+			detail = this.label.value;
+		}
+
+		// handle icon rendering
+		if (id.includes("slug")) {
 			iconPath = {
 				light: path.join(__filename, "..", "..", "media", "misc", "info.svg"),
 				dark: path.join(__filename, "..", "..", "media", "misc", "info.svg"),
 			}
 		}
-		if (text.includes("Downloads")) {
+		if (id.includes("downloads")) {
 			iconPath = {
 				light: path.join(__filename, "..", "..", "media", "misc", "downloads.svg"),
 				dark: path.join(__filename, "..", "..", "media", "misc", "downloads.svg"),
 			}
 		}
-		if (text.includes("Tag")) {
+		if (id.includes("tags")) {
 			iconPath = {
 				light: path.join(__filename, "..", "..", "media", "misc", "tags.svg"),
 				dark: path.join(__filename, "..", "..", "media", "misc", "tags.svg"),
 			}
 		}
-		if (text.includes("Version")) {
+		if (id.includes("version")) {
 			iconPath = {
 				light: path.join(__filename, "..", "..", "media", "misc", "version.svg"),
 				dark: path.join(__filename, "..", "..", "media", "misc", "version.svg"),
 			}
 		}
-		if (text.includes("Status")) {
-			if (text.includes("Quarantined")) {
+		if (id.includes("status")) {
+			if (id.includes("Quarantined")) { //if quarantined, flag it
 				iconPath = {
 					light: path.join(__filename, "..", "..", "media", "misc", "nope.svg"),
 					dark: path.join(__filename, "..", "..", "media", "misc", "nope.svg"),
@@ -52,21 +60,20 @@ class PackageDetailsNode {
 			}
 
 		}
-
-
+		
 		return {
-			label: detail,
+			label: value,
+			tooltip: id,
 			collapsibleState: vscode.TreeItemCollapsibleState.None, //no children to show so this cannot be extended
-			contextValue: "packageDetail",
-			iconPath: iconPath
+			conidValue: "packageDetail",
+			iconPath: iconPath,
+			
 		}
 	}
 
 	getChildren() {
 		return []
 	}
-
-
 
 }
 
--- models/packageNode.js
+++ models/packageNode.js
@@ -5,27 +5,25 @@ const packageDetailsNode = require('../models/packageDetailsNode')
 
 class PackageNode {
 	constructor(pkg) {
-			this.pkgDetails = [pkg.slug, pkg.slug_perm, pkg.downloads, pkg.version, pkg.status_str, pkg.tags];
-			this.slug = "Slug: " + pkg.slug;
-			this.slug_perm = "Slug Perm: " + pkg.slug_perm;
+			this.pkgDetails = [pkg.slug, pkg.slug_perm, pkg.downloads, pkg.version,  pkg.status_str, pkg.tags ];
+			this.slug = {"id": "slug", "value": pkg.slug};
+			this.slug_perm = {"id": "slug_perm", "value": pkg.slug_perm};
 			this.name = pkg.name;
-			this.status_str = "Status: " + pkg.status_str;
-			this.is_quarantined = pkg.is_quarantined;
-			this.downloads = "Downloads: " + pkg.downloads;
-			this.version = "Version: " + pkg.version;
+			this.status_str = {"id": "status", "value": pkg.status_str};
+			this.downloads = {"id": "downloads", "value": String(pkg.downloads)};
+			this.version = {"id": "version", "value": pkg.version};
 			this.format = pkg.format;
-			if(pkg.tags.info){
+			if(pkg.tags.info){ // handle tags since we split tags between tags.info and tags.version as both may not coexist at the same time
 				if(pkg.tags.version){
-					this.tags = "Tags: " + [pkg.tags.info, pkg.tags.version];
+					this.tags = {"id": "tags", "value": String([pkg.tags.info, pkg.tags.version])}; //combine tags sources
 				}
 				else {
-					this.tags = "Tags: " + pkg.tags.info;
+					this.tags = {"id": "tags", "value": pkg.tags.info};
 				}
 			}
 			else {
-				this.tags = "Tag: " + pkg.tags.version;
-			};
-		
+				this.tags = {"id": "tags", "value": pkg.tags.version};
+			};		
 	}
 
 	getTreeItem() {
@@ -33,7 +31,7 @@ class PackageNode {
 		let format = this.format
 		let pkg = this.name
 
-		// set package format icon. Using format var as filename so ensure any new icons added match the format naming convention and are svg files
+		// set package format icon. Using the format value as filename so ensure any new icons added match the format naming convention and are svg files. No need to hardcode logic for each type :) 
 		iconPath = {
 				light: path.join(__filename, "..", "..", "media", "formats", "light", format + '.svg'),
 				dark: path.join(__filename, "..", "..", "media", "formats", "dark", format + '.svg')
@@ -41,6 +39,7 @@ class PackageNode {
 
 		return {
 			label: pkg,
+			tooltip: format,
 			collapsibleState: vscode.TreeItemCollapsibleState.Collapsed,
 			contextValue: "package",
 			iconPath: iconPath
@@ -49,6 +48,7 @@ class PackageNode {
 
 	async getPackageDetails() {
 		let pkgDetails = this.pkgDetails
+		console.log(pkgDetails)
 		const PackageDetailsNodes = []
 		if (pkgDetails) {
 			for (const id of pkgDetails){
@@ -57,6 +57,7 @@ class PackageNode {
 				PackageDetailsNodes.push(packageDetailsNodeInst)
 			}			
 		}
+		console.log(PackageDetailsNodes)
 		return PackageDetailsNodes
 	}
 
